02c865932ac9a94db01de5aa904dc9220f3699a5
ShreyaAnde <shreyaande5@gmail.com>
2025-09-21 13:59:34 +0530

    log files

---
+++ ingestion_db.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "960f3bf9-f48f-4abd-91e9-ab0572032c38",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
